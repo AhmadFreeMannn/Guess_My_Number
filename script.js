5b60669dd063b7c4f482f5852b223a09f5d5181f
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-04-13 12:41:29 +0330

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent =
+  "😶‍🌫️ Correct Numberrrrr!!!!!!!!";
+
+document.querySelector(".number").textContent = "23";
+document.querySelector(".score").textContent = "24";
+
+document.querySelector(".guess").value = 10;
