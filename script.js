8729e8486971bd181e09b3f3166892846191d4e0
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-04-14 16:34:58 +0330

    addEventListeneer(دریافت عدد از کار بر و نمایش دادن پیام)

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 "use strict";
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent =
-  "😶‍🌫️ Correct Numberrrrr!!!!!!!!";
+// document.querySelector(".message").textContent =
+//   "😶‍🌫️ Correct Numberrrrr!!!!!!!!";
 
-document.querySelector(".number").textContent = "23";
-document.querySelector(".score").textContent = "24";
+// document.querySelector(".number").textContent = "23";
+// document.querySelector(".score").textContent = "24";
 
-document.querySelector(".guess").value = 10;
+// document.querySelector(".guess").value = 10;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guessNumber = Number(document.querySelector(".guess").value);
+
+  console.log(guessNumber, typeof guessNumber);
+
+  if (!guessNumber) {
+    document.querySelector(".message").textContent = "No Numberrr!!";
+  } else {
+    document.querySelector(".message").textContent = "Yeah this a numberrrr";
+  }
+});
