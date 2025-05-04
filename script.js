fa61d30c23b07c8ecd08168bdc33fe3fce251d68
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-05-04 23:56:57 +0330

    Fix: New comment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // random number
 document.querySelector(".number").textContent = secretNumber;
 
 let score = 20;
@@ -11,11 +11,13 @@ document.querySelector(".check").addEventListener("click", function () {
   console.log(guessNumber, typeof guessNumber);
 
   if (!guessNumber) {
+    // score and number comparison logic
     document.querySelector(".message").textContent = "No Numberrr!!";
   } else if (guessNumber === secretNumber) {
     document.querySelector(".message").textContent = "👍 correct number!";
   } else if (guessNumber < secretNumber) {
     if (score > 1) {
+      // to be sure when number is 0 the will stop
       document.querySelector(".message").textContent = "too low!";
       score--;
       document.querySelector(".score").textContent = score;
@@ -25,6 +27,7 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   } else {
     if (score > 1) {
+      // to be sure when number is 0 the will stop
       document.querySelector(".message").textContent = "too high!";
       score--;
       document.querySelector(".score").textContent = score;
