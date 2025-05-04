fdaee8a15cd5e64b2ac00a61891ef00d9597ef7a
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-05-04 23:54:54 +0330

    Fix: update score logic and number comparison in game logic

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 "use strict";
-// console.log(document.querySelector(".message").textContent);
 
-// document.querySelector(".message").textContent =
-//   "😶‍🌫️ Correct Numberrrrr!!!!!!!!";
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
-// document.querySelector(".number").textContent = "23";
-// document.querySelector(".score").textContent = "24";
-
-// document.querySelector(".guess").value = 10;
-
-const random_number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = random_number;
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guessNumber = Number(document.querySelector(".guess").value);
@@ -19,7 +12,25 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guessNumber) {
     document.querySelector(".message").textContent = "No Numberrr!!";
+  } else if (guessNumber === secretNumber) {
+    document.querySelector(".message").textContent = "👍 correct number!";
+  } else if (guessNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game";
+      document.querySelector(".score").textContent = "0";
+    }
   } else {
-    document.querySelector(".message").textContent = "Yeah this is a numberrrr";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game";
+      document.querySelector(".score").textContent = "0";
+    }
   }
 });
