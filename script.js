121d4462e0b5704e6b752a5f7f4d717e763a8f34
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-05-06 00:53:47 +0330

    Fix : High score fixed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // random number
 
 let score = 20;
+let highScoreTemp = 0;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guessNumber = Number(document.querySelector(".guess").value);
@@ -21,12 +23,23 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").style.width = "35rem";
     document.querySelector(".number").textContent = secretNumber;
 
+    highScore = Number(
+      (document.querySelector(".highscore").value = highScoreTemp)
+    );
+    if (highScore > highScoreTemp) {
+      document.querySelector(".highscore").textContent = highScore;
+    } else {
+      document.querySelector(".highscore").textContent = highScoreTemp;
+    }
+
     // to be sure when number is 0 the will stop & guess is too high
   } else if (guessNumber < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "too low!";
       score--;
       document.querySelector(".score").textContent = score;
+
+      highScoreTemp = score;
     } else {
       document.querySelector(".message").textContent = "you lost the game";
       document.querySelector(".score").textContent = "0";
@@ -37,6 +50,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "too high!";
       score--;
       document.querySelector(".score").textContent = score;
+
+      highScoreTemp = score;
     } else {
       document.querySelector(".message").textContent = "you lost the game";
       document.querySelector(".score").textContent = "0";
