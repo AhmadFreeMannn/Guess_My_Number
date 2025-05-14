21fdca95b4b6018aacde3c164f480a7d6c4e5f32
AhmadReza <ahmadreza.mirzaei03@gmail.com>
2025-05-14 10:34:34 +0330

    Fix : high score fix-2

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // random number
 
 let score = 20;
-let highScoreTemp = 0;
 let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -23,13 +22,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").style.width = "35rem";
     document.querySelector(".number").textContent = secretNumber;
 
-    highScore = Number(
-      (document.querySelector(".highscore").value = highScoreTemp)
-    );
-    if (highScore > highScoreTemp) {
+    document.querySelector(".highscore").textContent = highScore;
+
+    if (highScore < score) {
+      highScore = score;
       document.querySelector(".highscore").textContent = highScore;
-    } else {
-      document.querySelector(".highscore").textContent = highScoreTemp;
     }
 
     // to be sure when number is 0 the will stop & guess is too high
@@ -39,8 +36,6 @@ document.querySelector(".check").addEventListener("click", function () {
         guessNumber < secretNumber ? "too low!" : "too high!";
       score--;
       document.querySelector(".score").textContent = score;
-
-      highScoreTemp = score;
     } else {
       document.querySelector(".message").textContent = "you lost the game";
       document.querySelector(".score").textContent = "0";
